8eca452aed6c6119f5da8ab687dd9f45cbf43c08
Ironious4 <tyronekariuki@gmail.com>
2024-08-09 09:21:05 +0300

    Add code-along

---
+++ src/pages/UserProfile.js
@@ -0,0 +1,36 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import {useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
+
+function UserProfile() {
+   const [user,setUser]= useState({});
+   const params= useParams() ;
+   const userId= params.id;
+
+   useEffect(()=> {
+    fetch(`https://localhost:4000/users/${userId}`)
+    .then(r=>r.json())
+    .then(data=> setUser(data))
+    .catch(error=>console.error(error));
+   }, [userId]);
+
+   if(!user.name) {
+    return <h1>Loading...</h1>;
+   };
+
+    return(
+        <>
+        <header>
+            <NavBar/>
+        </header>
+        <main>
+            <h1>User Profile</h1>
+        </main>
+        </>
+    )
+
+};
+
+export default UserProfile;
+
